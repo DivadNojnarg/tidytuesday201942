869681147fbed505030c218c2201116f4a05a9e4
DivadNojnarg <dgranjon@ymail.com>
2022-04-28 01:31:41 +0200

    add validator

--- R/run_app.R
+++ R/run_app.R
@@ -1,13 +1,25 @@
-#' Run the Shiny Application
+#' Run example app
 #'
-#' @param ... list of golem options
-#' 
+#' @return A shiny app object
 #' @export
-#' @importFrom shiny shinyApp
-#' @importFrom golem with_golem_options
-run_app <- function(...) {
-  with_golem_options(
-    app = shinyApp(ui = app_ui, server = app_server), 
-    golem_opts = list(...)
-  )
+#' @import shiny
+run_app <- function() {
+  # serve js tools for Monkey test (in case proxy blocks external scripts)
+  addResourcePath("gremlins", "inst/shinyValidator-js")
+  # DON'T CHANGE (INTERNAL TO SHINYVALIDATOR)
+  if (!exists(".enable_reactlog")) .enable_reactlog <- FALSE
+  if (!exists(".profile_code")) .profile_code <- FALSE
+
+  if (.enable_reactlog || .profile_code) {
+    tmp <- body(app_server)
+    start <- length(tmp) + 1 # start just before the closing }
+    body(app_server)[[start]] <- substitute(
+      onSessionEnded(function() {
+        stopApp(reactlog())
+      })
+    )
+  }
+  runApp(shinyApp(app_ui, app_server))
 }
+
+globalVariables(c("app_ui", "app_server"))
